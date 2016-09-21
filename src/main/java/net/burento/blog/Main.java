d2a1f5d24385849da4b868fef3726b4ffdb401bf
Brent Perry <burentoperii@gmail.com>
2016-09-21 14:17:06 -0500

    Add HTML/CSS files

--- src/main/java/net/burento/blog/Main.java
+++ src/main/java/net/burento/blog/Main.java
@@ -1,7 +1,9 @@
 package net.burento.blog;
 
+import static spark.Spark.get;
+
 public class Main {
     public static void main(String[] args) {
-
+        get("/", (req, res) -> "Brent's blog");
     }
 }
